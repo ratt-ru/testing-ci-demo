a47ae40693bf0b8283e588903342cad817fb1d3c
Simon Perkins <simon.perkins@gmail.com>
2020-09-29 12:01:28 +0200

    pytest parametrize f

--- testingcidemo/tests/test_pytest.py
+++ testingcidemo/tests/test_pytest.py
@@ -3,18 +3,7 @@ import pytest
 
 from testingcidemo import f
 
-def test_f():
-    assert f(0) == 0
-    assert f(1) == 1
-    assert f(2) == 2
-    assert f(3) == 3
-    assert f(4) == "Apples"
-    assert f(5) == 5
-    assert f(6) == 6
-    assert f(7) == 7
-    assert f(8) == 8
-    assert f(9) == 9
-    assert f(10) == 10
 
-    with pytest.raises(ValueError):
-        assert f(11) == 11
+@pytest.mark.parametrize("i", range(12))
+def test_f(i):
+    assert f(i) == i
