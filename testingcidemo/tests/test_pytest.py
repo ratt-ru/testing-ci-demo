8a7a85a768a1aad41ab45cf69ef0666d15e8bd88
Simon Perkins <simon.perkins@gmail.com>
2020-09-29 11:56:53 +0200

    Test full range

--- testingcidemo/tests/test_pytest.py
+++ testingcidemo/tests/test_pytest.py
@@ -1,10 +1,20 @@
+import pytest
+
 
 from testingcidemo import f
 
 def test_f():
     assert f(0) == 0
     assert f(1) == 1
-
-
-def test_f2():
     assert f(2) == 2
+    assert f(3) == 3
+    assert f(4) == 4
+    assert f(5) == 5
+    assert f(6) == 6
+    assert f(7) == 7
+    assert f(8) == 8
+    assert f(9) == 9
+    assert f(10) == 10
+
+    with pytest.raises(ValueError):
+        assert f(11) == 11
