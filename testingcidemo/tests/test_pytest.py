bb3d182ae530d00a210e84c65bc48a2452a61cbe
Simon Perkins <simon.perkins@gmail.com>
2020-09-29 12:10:02 +0200

    Parametrizations

--- testingcidemo/tests/test_pytest.py
+++ testingcidemo/tests/test_pytest.py
@@ -4,6 +4,27 @@ import pytest
 from testingcidemo import f
 
 
-@pytest.mark.parametrize("i", range(12))
-def test_f(i):
+@pytest.mark.parametrize("i", list(range(0, 4)) + list(range(5, 11)))
+def test_f_success(i):
     assert f(i) == i
+
+
+@pytest.mark.parametrize("i", [4])
+def test_f_4(i):
+    assert f(i) == "Apples"
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize("i", [-1, 11])
+def test_f_4(i):
+    assert f(i) == i
+
+
+@pytest.mark.parametrize("i, o", [
+    (0, 0),
+    (1, 1),
+    (4, "Apples"),
+    pytest.param(11, 11, marks=pytest.mark.xfail),
+])
+def test_f_success_another(i, o):
+    assert f(i) == o
